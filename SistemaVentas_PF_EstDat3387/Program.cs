2613559e19e572a40ff7cb62d8e96d371a461188
FABIANCMQ <fab.castmurga@gmail.com>
2025-10-31 21:39:13 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVentas_PF_EstDat3387/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas_PF_EstDat3387
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
