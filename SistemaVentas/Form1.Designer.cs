adce0b44aa6f7cf46c671bba0b82fd9197e2757f
FABIANCMQ <fab.castmurga@gmail.com>
2025-11-07 14:44:08 -0500

    Avance Windows Forms

--- SistemaVentas/Form1.Designer.cs
+++ SistemaVentas/Form1.Designer.cs
@@ -28,13 +28,269 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPlataformas = new System.Windows.Forms.DataGridView();
+            this.Plataforma = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Contraseña = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btRegistrarPlataforma = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbPlataformas = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlataformas)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dgvPlataformas);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(583, 267);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gestión de Plataformas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cuentas: ";
+            // 
+            // dgvPlataformas
+            // 
+            this.dgvPlataformas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlataformas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Plataforma,
+            this.Usuario,
+            this.Contraseña,
+            this.Precio});
+            this.dgvPlataformas.Location = new System.Drawing.Point(6, 55);
+            this.dgvPlataformas.Name = "dgvPlataformas";
+            this.dgvPlataformas.RowHeadersWidth = 51;
+            this.dgvPlataformas.RowTemplate.Height = 24;
+            this.dgvPlataformas.Size = new System.Drawing.Size(553, 150);
+            this.dgvPlataformas.TabIndex = 0;
+            // 
+            // Plataforma
+            // 
+            this.Plataforma.HeaderText = "Plataforma";
+            this.Plataforma.MinimumWidth = 6;
+            this.Plataforma.Name = "Plataforma";
+            this.Plataforma.Width = 125;
+            // 
+            // Usuario
+            // 
+            this.Usuario.HeaderText = "Usuario";
+            this.Usuario.MinimumWidth = 6;
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Width = 125;
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.HeaderText = "Contraseña";
+            this.Contraseña.MinimumWidth = 6;
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Width = 125;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 6;
+            this.Precio.Name = "Precio";
+            this.Precio.Width = 125;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btRegistrarPlataforma);
+            this.groupBox2.Controls.Add(this.txtPrecio);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtContrasena);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtUsuario);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cbPlataformas);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(602, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(229, 267);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Registro de Plataformas";
+            // 
+            // btRegistrarPlataforma
+            // 
+            this.btRegistrarPlataforma.Location = new System.Drawing.Point(50, 197);
+            this.btRegistrarPlataforma.Name = "btRegistrarPlataforma";
+            this.btRegistrarPlataforma.Size = new System.Drawing.Size(121, 51);
+            this.btRegistrarPlataforma.TabIndex = 8;
+            this.btRegistrarPlataforma.Text = "Registrar Plataforma";
+            this.btRegistrarPlataforma.UseVisualStyleBackColor = true;
+            this.btRegistrarPlataforma.Click += new System.EventHandler(this.btRegistrarPlataforma_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(92, 159);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(121, 22);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Precio: ";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(92, 125);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(121, 22);
+            this.txtContrasena.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Contraseña: ";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(92, 90);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(121, 22);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario: ";
+            // 
+            // cbPlataformas
+            // 
+            this.cbPlataformas.FormattingEnabled = true;
+            this.cbPlataformas.Items.AddRange(new object[] {
+            "Netflix",
+            "HBO",
+            "Disney",
+            "Prime Video"});
+            this.cbPlataformas.Location = new System.Drawing.Point(92, 33);
+            this.cbPlataformas.Name = "cbPlataformas";
+            this.cbPlataformas.Size = new System.Drawing.Size(121, 24);
+            this.cbPlataformas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Plataforma: ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.dgvClientes);
+            this.groupBox3.Location = new System.Drawing.Point(13, 286);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(583, 267);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cola de Clientes";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Clientes:";
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(6, 55);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.RowHeadersWidth = 51;
+            this.dgvClientes.RowTemplate.Height = 24;
+            this.dgvClientes.Size = new System.Drawing.Size(553, 150);
+            this.dgvClientes.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1044, 589);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlataformas)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvPlataformas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbPlataformas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btRegistrarPlataforma;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Plataforma;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Contraseña;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvClientes;
     }
 }
 
--- SistemaVentas/Form1.cs
+++ SistemaVentas/Form1.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BibliotecaClases;
 
 namespace SistemaVentas
 {
+
     public partial class Form1 : Form
     {
+        public Listas lsInventario = new Listas();
+        public Pilas plNetflix = new Pilas();
+        public Pilas plHBO = new Pilas();
+        public Pilas plDisney = new Pilas();
+        public Pilas plPrime = new Pilas();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btRegistrarPlataforma_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "" || txtContrasena.Text == "" || txtPrecio.Text == "")
+            {
+                MessageBox.Show("Complete todos los campos", "Aviso");
+                return;
+            }
+
+            double precio;
+            if (!double.TryParse(txtPrecio.Text, out precio))
+            {
+                MessageBox.Show("Ingrese un precio valido.", "Error");
+                return;
+            }                                                      
+
+            Cuenta nuevaCuenta = new Cuenta(cbPlataformas.Text, txtUsuario.Text, txtContrasena.Text, double.Parse(txtPrecio.Text));
+            lsInventario.Insertar(nuevaCuenta);
+            txtUsuario.Clear();
+            txtContrasena.Clear();
+            txtPrecio.Clear();
+            MessageBox.Show("Cuenta Agregada", "Éxito");
+            mostrarCuentas();
+        }
+        public void mostrarCuentas()
+        {
+            dgvPlataformas.Rows.Clear();
+            Nodo temp = lsInventario.primero;
+            while (temp != null)
+            {
+                dgvPlataformas.Rows.Add(temp.dato.Plataforma, temp.dato.Usuario, temp.dato.Contraseña, temp.dato.Precio);
+                temp = temp.sig;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvPlataformas.Columns.Add("Plataform", "Plataforma");
+            dgvPlataformas.Columns.Add("User", "Usuario");
+            dgvPlataformas.Columns.Add("Password", "Contraseña");
+            dgvPlataformas.Columns.Add("Price", "Precio");
+
+            dgvClientes.Columns.Add("DNI","DNI");
+            dgvClientes.Columns.Add("Name","Nombre");
+            dgvClientes.Columns.Add("Phone","Teléfono");
         }
     }
 }
